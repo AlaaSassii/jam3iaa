fdee6de812a68913108fffb0037d67a1eb0d7268
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-03-27 14:26:43 +0000

    event ,activity pages

--- app/home/events/page.jsx
+++ app/home/events/page.jsx
@@ -1,41 +1,74 @@
-"use client";
-import React, { useEffect } from "react";
+'use client';
+import React, { useEffect, useState } from "react";
 import { useCreateEvent } from "../../../hooks/Store/useCreateEvent";
 import { getEvents } from "../../../firebase/event";
 import EventsCard from "../../../components/EventsCart";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
-const page = () => {
+
+const Page = () => {
+  
   const { events, getEvent } = useCreateEvent((state) => ({
     events: state.events,
     getEvent: state.getEvent,
   }));
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredEvents, setFilteredEvents] = useState([]);
+
   useEffect(() => {
     getEvents()
       .then((data) => {
         getEvent(data);
+        setFilteredEvents(data); // Initialize filteredEvents with all events
       })
       .catch((err) => console.log(err));
   }, []);
+
+  useEffect(() => {
+    // Filter events based on searchQuery, title, address, and description
+    const filtered = events.filter((event) => {
+        const eventName = event.name ? event.name.toLowerCase() : ""; // Check if event name exists
+        const eventAddress = event.address ? event.address.toLowerCase() : ""; // Check if event address exists
+        const eventDescription = event.description ? event.description.toLowerCase() : ""; // Check if event description exists
+        const eventDate = event.date ? event.date.toString() : ""; // Convert date to string and check if it exists
+
+        return (
+            eventName.includes(searchQuery.toLowerCase()) || // Check if event name includes search query
+            eventAddress.includes(searchQuery.toLowerCase()) || // Check if event address includes search query
+            eventDescription.includes(searchQuery.toLowerCase()) || // Check if event description includes search query
+            eventDate.includes(searchQuery.toLowerCase()) // Check if event date includes search query
+        );
+    });
+    setFilteredEvents(filtered);
+}, [searchQuery, events]);
+
   return (
     <>
       <Navbar />
-
-      <div>
-        {events?.map((e, i) => (
-          <EventsCard
-            title={e.name}
-            path={e.image}
-            adress={e.adress}
-            disc={e.description}
-            link={`events/${e.id}`}
-            key={i}
-          />
-        ))}
+      <div className="flex flex-col justify-center items-center gap-5 mt-20">
+        <div>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" onChange={(e) => setSearchQuery(e.target.value)} value={searchQuery} />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+          </label>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-4  z-1">
+          {filteredEvents.map((e, i) => (
+            <EventsCard
+              title={e.name}
+              path={e.image}
+              address={e.address}
+              disc={e.description}
+              link={`events/${e.id}`}
+              key={i}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
   );
 };
 
-export default page;
+export default Page;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,25 +20,25 @@ const Navbar = () => {
 
     return (
         <div className=''>
-            <div className="flex justify-between md:justify-around  items-center bg-rose-200">
+            <div className="flex justify-between lg:justify-around  items-center bg-gray-800 z-100000">
                 <img
                 src={'/images/logo.png'}
                 width={80}
                 height={50}
                 alt='logo'
-                className='p-1'
+                className='p-1 mx-2'
                 />
                 
-                <ul className={`absolute flex-col bg-neutral-800 p-10 gap-5 top-0 w-full md:w-auto md:flex-row md:bg-transparent md:p-2 md:gap-0 md:relative flex justify-center items-center text-rose-600 md:text-rose-600 font-semibold ${isMenuOpen ? '' : 'hidden '}`}>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="#home">Accueil</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="#about">À propos nous</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="#events">événements</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="#activity">activités</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="#contact">Contactez-nous</a></li>
-                    <IoMdClose size={30} className="md:hidden absolute top-5 right-5 " onClick={toggleMenu}/>
+                <ul className={` flex-col bg-neutral-800 p-10 gap-5 top-0 text-sm lg:text-base w-full lg:w-auto lg:flex-row lg:bg-transparent lg:p-2 lg:gap-0 lg:relative flex justify-center items-center text-rose-600 lg:text-rose-600 font-semibold fixed ${isMenuOpen ? '' : 'hidden '}`}>
+                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#home">Accueil</a></li>
+                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#about">À propos nous</a></li>
+                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home/events">événements</a></li>
+                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home/activities">activités</a></li>
+                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#contact">Contactez-nous</a></li>
+                    <IoMdClose size={30} className="lg:hidden absolute top-5 right-5 " onClick={toggleMenu}/>
                 </ul>
-                <div className="hidden md:block bg-white  p-3 my-2 rounded-2xl font-semibold cursor-pointer hover:text-rose-500 " onClick={()=>{document.getElementById('my_modal_1').showModal()}}>Contactez nous</div>
-                <IoMenu className="md:hidden text-black mx-2" onClick={toggleMenu} size={35}/>
+                <div className="hidden lg:block bg-white text-sm  p-2 my-2 rounded-2xl font-semibold cursor-pointer hover:text-rose-500 " onClick={()=>{document.getElementById('my_modal_1').showModal()}}>Contactez nous</div>
+                <IoMenu className="lg:hidden text-white mx-2" onClick={toggleMenu} size={35}/>
             </div>
         </div>
     );
