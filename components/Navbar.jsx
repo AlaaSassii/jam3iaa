5d17237719fa9af9bb30248d412b1495c9958694
alaasassii <alaasassijs@gmail.com>
2024-03-28 00:53:13 +0100

    add language

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,47 +1,96 @@
-'use client'
-import React, { useState , useEffect} from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
-
-
+import { components } from "../lang";
+import { useLanguage } from "../hooks/useLanguage";
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const { language, setLanguage } = useLanguage();
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    useEffect(()=>{
-        if(window.innerWidth < 722){
-            setIsMenuOpen(false)
-        }},[])
+  useEffect(() => {
+    if (window.innerWidth < 722) {
+      setIsMenuOpen(false);
+    }
+  }, []);
+  return (
+    <div className=''>
+      <div className='flex justify-between lg:justify-around  items-center bg-gray-800 z-100000'>
+        <img
+          src={"/images/logo.png"}
+          width={80}
+          height={50}
+          alt='logo'
+          className='p-1 mx-2'
+        />
 
-    
-
-    return (
-        <div className=''>
-            <div className="flex justify-between lg:justify-around  items-center bg-gray-800 z-100000">
-                <img
-                src={'/images/logo.png'}
-                width={80}
-                height={50}
-                alt='logo'
-                className='p-1 mx-2'
-                />
-                
-                <ul className={` flex-col bg-neutral-800 p-10 gap-5 top-0 text-sm lg:text-base w-full lg:w-auto lg:flex-row lg:bg-transparent lg:p-2 lg:gap-0 lg:relative flex justify-center items-center text-rose-600 lg:text-rose-600 font-semibold fixed ${isMenuOpen ? '' : 'hidden '}`}>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#home">Accueil</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#about">À propos nous</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home/events">événements</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home/activities">activités</a></li>
-                    <li className="cursor-pointer px-2 hover:text-black transition"><a href="http://localhost:3000/home#contact">Contactez-nous</a></li>
-                    <IoMdClose size={30} className="lg:hidden absolute top-5 right-5 " onClick={toggleMenu}/>
-                </ul>
-                <div className="hidden lg:block bg-white text-sm  p-2 my-2 rounded-2xl font-semibold cursor-pointer hover:text-rose-500 " onClick={()=>{document.getElementById('my_modal_1').showModal()}}>Contactez nous</div>
-                <IoMenu className="lg:hidden text-white mx-2" onClick={toggleMenu} size={35}/>
-            </div>
+        <ul
+          className={` flex-col bg-neutral-800 p-10 gap-5 top-0 text-sm lg:text-base w-full lg:w-auto lg:flex-row lg:bg-transparent lg:p-2 lg:gap-0 lg:relative flex justify-center items-center text-rose-600 lg:text-rose-600 font-semibold fixed ${
+            isMenuOpen ? "" : "hidden"
+          }`}
+        >
+          <li className='cursor-pointer px-2 hover:text-black transition'>
+            <a href='http://localhost:3000/home#home'>
+              {components.Navbar?.[language].Accueil}
+            </a>
+          </li>
+          <li className='cursor-pointer px-2 hover:text-black transition'>
+            <a href='http://localhost:3000/home#about'>
+              {components.Navbar?.[language].aProposNous}
+            </a>
+          </li>
+          <li className='cursor-pointer px-2 hover:text-black transition'>
+            <a href='http://localhost:3000/home/events'>
+              {components.Navbar.ar.Événements}
+            </a>
+          </li>
+          <li className='cursor-pointer px-2 hover:text-black transition'>
+            <a href='http://localhost:3000/home/activities'>
+              {components.Navbar.ar.Activités}
+            </a>
+          </li>
+          <li className='cursor-pointer px-2 hover:text-black transition'>
+            <a href='http://localhost:3000/home#contact'>
+              {components.Navbar.ar.contactezNous}
+            </a>
+          </li>
+          <IoMdClose
+            size={30}
+            className='lg:hidden absolute top-5 right-5 '
+            onClick={toggleMenu}
+          />
+        </ul>
+        <select
+          defaultValue={language}
+          onChange={(e) => {
+            setLanguage(e.target.value);
+            console.log({ language: e.target.value });
+          }}
+        >
+          <option value='fr'>Francais</option>
+          <option value='ar'>العربية</option>
+        </select>
+        <div
+          className='hidden lg:block bg-white text-sm  p-2 my-2 rounded-2xl font-semibold cursor-pointer hover:text-rose-500 '
+          onClick={() => {
+            document.getElementById("my_modal_1").showModal();
+          }}
+        >
+          Contactez nous
         </div>
-    );
-}
+
+        <IoMenu
+          className='lg:hidden text-white mx-2'
+          onClick={toggleMenu}
+          size={35}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
---
+++ lang.js
@@ -0,0 +1,37 @@
+export const components = {
+    'Navbar': {
+        ar: {
+            Accueil: 'الصفحة الرئيسية',
+            aProposNous: 'من نحن',
+            Événements: 'الفعاليات',
+            Activités: 'الأنشطة',
+            contactezNous: 'اتصل بنا'
+        },
+        fr: {
+            Accueil: 'Accueil',
+            aProposNous: 'À propos nous',
+            Événements: 'Événements',
+            Activités: 'Activités',
+            contactezNous: 'Contactez-nous'
+        }
+    },
+    'About': {
+        fr: {
+
+        },
+        ar: {
+
+        },
+    },
+    'activity': {
+        fr: {
+
+        },
+        ar: {
+
+        },
+    }
+
+}
+
+// components.Navbar.['À propos nous'].Accueil.text
