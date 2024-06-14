6a86dd6380c1e5da5d0fab5777432b75679b747b
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-06-14 09:54:38 +0100

    asdasd

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,9 +7,9 @@ import { useLanguage } from "../hooks/useLanguage";
 
 const Home = ({ language }) => {
   return (
-    <div className='w-full bg-black h-screen home-container' id='home'>
+    <div className="w-full bg-black h-screen home-container" id="home">
       <div
-        className='
+        className="
                 text-center
                 px-2
                 md:px-20
@@ -18,19 +18,19 @@ const Home = ({ language }) => {
                 flex
                 flex-col
                 items-center
-                justify-center'
+                justify-center"
       >
-        <h1 className=' text-white text-[30px]  md:text-[50px] font-extrabold'>
+        <h1 className=" text-white text-[30px]  md:text-[50px] font-extrabold">
           {components.HomeC?.[language].Title}
         </h1>
-        <p className='text-neutral-300 font-bold mt-2 '>
+        <p className="text-neutral-300 font-bold mt-2 ">
           {components.HomeC?.[language].secondTitle}
         </p>
-        <p className='text-neutral-300 font-bold mt-2'>
+        <p className="text-neutral-300 font-bold mt-2">
           {components.HomeC?.[language].thirdTitle}
         </p>
         <button
-          className='p-3 text-sm md:text-base rounded-xl bg-rose-500 text-white mt-4 font-bold cursor-pointer hover:bg-rose-600 flex items-center gap-3'
+          className="p-3 text-sm md:text-base rounded-xl bg-rose-500 text-white mt-4 font-bold cursor-pointer hover:bg-rose-600 flex items-center gap-3"
           onClick={() => {
             document.getElementById("my_modal_1").showModal();
           }}
--- components/Valunteer.jsx
+++ components/Valunteer.jsx
@@ -3,67 +3,92 @@ import React from "react";
 
 import { components } from "../lang";
 import { useLanguage } from "../hooks/useLanguage";
+import Image from "next/image";
 
 const Valunteer = ({ language }) => {
   return (
-    <div className='flex flex-col items-center gap-4 my-20'>
-      <div className='font-bold text-lg md:text-4xl'>
+    <div className="flex flex-col items-center gap-4 my-20">
+      <div className="font-bold text-lg md:text-4xl">
         {" "}
         {components.Valunteer?.[language].bigTitle}{" "}
       </div>
 
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-10 mx-5 '>
-        <div className='flex flex-col items-center gap-3 cursor-pointer'>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-10 mx-5 ">
+        <div className="flex flex-col items-center gap-3 cursor-pointer">
           <div className="w-72 h-72">
-            <img src='avatar/11.jpg' className='object-cover w-full h-full' alt='' />
+            <Image
+              src="/avatar/11.jpg"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt=""
+            />
           </div>
-          <div className='flex flex-col items-center gap-0'>
-            <div className='font-bold text-xs md:text-lg'>
+          <div className="flex flex-col items-center gap-0">
+            <div className="font-bold text-xs md:text-lg">
               {components.Valunteer?.[language].title1}
             </div>
-            <div className='text-xs md:text-lg'>
+            <div className="text-xs md:text-lg">
               {components.Valunteer?.[language].discription1}
             </div>
           </div>
         </div>
 
-        <div className='flex flex-col items-center gap-3 cursor-pointer'>
+        <div className="flex flex-col items-center gap-3 cursor-pointer">
           <div className="w-72 h-72">
-            <img src='avatar/22.jpg' className='object-cover w-full h-full' alt='' />
+            <Image
+              src="/avatar/33.jpg"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt=""
+            />
           </div>
-          <div className='flex flex-col items-center gap-0'>
-            <div className='font-bold text-xs md:text-lg'>
+          <div className="flex flex-col items-center gap-0">
+            <div className="font-bold text-xs md:text-lg">
               {components.Valunteer?.[language].title2}
             </div>
-            <div className='text-xs md:text-lg'>
+            <div className="text-xs md:text-lg">
               {components.Valunteer?.[language].discription2}
             </div>
           </div>
         </div>
 
-        <div className='flex flex-col items-center gap-3  cursor-pointer'>
+        <div className="flex flex-col items-center gap-3  cursor-pointer">
           <div className="w-72 h-72">
-            <img src='avatar/33.jpg' className='object-cover w-full h-full' alt='' />
+            <Image
+              src="/avatar/22.jpg"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt=""
+            />
           </div>
-          <div className='flex flex-col items-center gap-0'>
-            <div className='font-bold text-xs md:text-lg'>
+          <div className="flex flex-col items-center gap-0">
+            <div className="font-bold text-xs md:text-lg">
               {components.Valunteer?.[language].title3}
             </div>
-            <div className='text-xs md:text-lg'>
+            <div className="text-xs md:text-lg">
               {components.Valunteer?.[language].discription3}
             </div>
           </div>
         </div>
 
-        <div className='flex flex-col items-center gap-3  cursor-pointer'>
+        <div className="flex flex-col items-center gap-3  cursor-pointer">
           <div className="w-72 h-72">
-            <img src='avatar/44.jpg' className='object-cover w-full h-full' alt='' />
+            <Image
+              src="/avatar/44.jpg"
+              width={500}
+              height={500}
+              className="object-cover w-full h-full"
+              alt=""
+            />
           </div>
-          <div className='flex flex-col items-center gap-0'>
-            <div className='font-bold text-xs md:text-lg'>
+          <div className="flex flex-col items-center gap-0">
+            <div className="font-bold text-xs md:text-lg">
               {components.Valunteer?.[language].title4}
             </div>
-            <div className='text-xs md:text-lg'>
+            <div className="text-xs md:text-lg">
               {components.Valunteer?.[language].discription4}
             </div>
           </div>
