dd0c7480f8816d757dcb46e866e3a50fa57e0223
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-06-20 12:25:08 +0100

    asdfqW

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -71,58 +71,58 @@ const Contact = ({ language }) => {
 
   return (
     <div
-      className='flex flex-col items-center gap-4 mt-20 bg-rose-100 pb-16'
-      id='contact'
+      className="flex flex-col items-center gap-4 mt-20 bg-rose-100 pb-16"
+      id="contact"
     >
-      <div className='w-full mb-8 pb-8 bg-white'>
-        <h1 className='mb-4 text-center text-3xl font-bold '>
+      <div className="w-full mb-8 pb-8 bg-white">
+        <h1 className="mb-4 text-center text-3xl font-bold ">
           {language === "ar" ? "الموقع" : "l'emplacement"}
         </h1>
         <iframe
-          src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12930.4256683772!2d10.538313!3d35.8831609!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd8bd158f4f4d3%3A0xf47811755f1a24e5!2zQXNzb2NpYXRpb24gQXJpaiAtINis2YXYudmK2ZHYqSDYo9ix2YrYrA!5e0!3m2!1sen!2stn!4v1717850477429!5m2!1sen!2stn'
-          allowfullscreen=''
-          loading='lazy'
-          referrerpolicy='no-referrer-when-downgrade'
-          className='w-[80%] mx-auto h-[600px]'
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12930.4256683772!2d10.538313!3d35.8831609!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd8bd158f4f4d3%3A0xf47811755f1a24e5!2zQXNzb2NpYXRpb24gQXJpaiAtINis2YXYudmK2ZHYqSDYo9ix2YrYrA!5e0!3m2!1sen!2stn!4v1717850477429!5m2!1sen!2stn"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-[80%] mx-auto h-[600px]"
         ></iframe>
       </div>
-      <div className='font-bold text-lg md:text-4xl'>
+      <div className="font-bold text-lg md:text-4xl">
         {components.contact?.[language].contactnous}
       </div>
-      <div className='w-full '>
-        <div className='max-w-3xl mx-auto mt-8 flex flex-col md:flex-row  justify-between items-center gap-10  '>
-          <div className='w-full p-3 md:pr-8'>
-            <form ref={form} onSubmit={sendEmail} className='space-y-4'>
+      <div className="w-full ">
+        <div className="max-w-3xl mx-auto mt-8 flex flex-col md:flex-row  justify-between items-center gap-10  ">
+          <div className="w-full p-3 md:pr-8">
+            <form ref={form} onSubmit={sendEmail} className="space-y-4">
               <div>
-                <label htmlFor='email' className='block mb-1'>
+                <label htmlFor="email" className="block mb-1">
                   {components.contact?.[language].email}
                 </label>
                 <input
-                  type='email'
-                  name='user_email'
-                  className='w-full p-2 rounded'
+                  type="email"
+                  name="user_email"
+                  className="w-full p-2 rounded"
                 />
               </div>
               <div>
-                <label htmlFor='title' className='block mb-1'>
+                <label htmlFor="title" className="block mb-1">
                   {components.contact?.[language].titre}
                 </label>
                 <input
-                  type='text'
-                  name='user_name'
-                  className='w-full p-2 rounded'
+                  type="text"
+                  name="user_name"
+                  className="w-full p-2 rounded"
                 />
               </div>
               <div>
-                <label htmlFor='description' className='block mb-1'>
+                <label htmlFor="description" className="block mb-1">
                   {components.contact?.[language].description}
                 </label>
-                <textarea name='message' className='w-full p-2 rounded' />
+                <textarea name="message" className="w-full p-2 rounded" />
               </div>
               <input
-                type='submit'
+                type="submit"
                 value={language === "ar" ? "إرسال" : "Envoyer"}
-                className='w-full py-2 bg-rose-500 text-white rounded-md hover:bg-rose-600'
+                className="w-full py-2 bg-rose-500 text-white rounded-md hover:bg-rose-600"
               />
             </form>
           </div>
--- components/Valunteer.jsx
+++ components/Valunteer.jsx
@@ -12,9 +12,9 @@ const Valunteer = ({ language }) => {
         {components.Valunteer?.[language].bigTitle}{" "}
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10 mx-5 ">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-10 mx-3 ">
         <div className="flex flex-col items-center gap-3 cursor-pointer">
-          <div className="w-72 h-72">
+          <div className="w-44 h-44 lg:w-52 lg:h-60">
             <img
               src="avatar/11.jpg"
               className="object-cover w-full h-full"
@@ -22,35 +22,17 @@ const Valunteer = ({ language }) => {
             />
           </div>
           <div className="flex flex-col items-center gap-0">
-            <div className="font-bold text-xs md:text-lg">
+            <div className="font-bold text-xs md:text-sm">
               {components.Valunteer?.[language].title1}
             </div>
-            <div className="text-xs md:text-lg">
+            <div className="text-xs md:text-sm">
               {components.Valunteer?.[language].discription1}
             </div>
           </div>
         </div>
 
         <div className="flex flex-col items-center gap-3 cursor-pointer">
-          <div className="w-72 h-72">
-            <img
-              src="avatar/22.jpg"
-              className="object-cover w-full h-full"
-              alt=""
-            />
-          </div>
-          <div className="flex flex-col items-center gap-0">
-            <div className="font-bold text-xs md:text-lg">
-              {components.Valunteer?.[language].title2}
-            </div>
-            <div className="text-xs md:text-lg">
-              {components.Valunteer?.[language].discription2}
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col items-center gap-3  cursor-pointer">
-          <div className="w-72 h-72">
+          <div className="w-44 h-44 lg:w-52 lg:h-60">
             <img
               src="avatar/44.jpg"
               className="object-cover w-full h-full"
@@ -58,17 +40,35 @@ const Valunteer = ({ language }) => {
             />
           </div>
           <div className="flex flex-col items-center gap-0">
-            <div className="font-bold text-xs md:text-lg">
+            <div className="font-bold text-xs md:text-sm">
+              {components.Valunteer?.[language].title2}
+            </div>
+            <div className="text-xs md:text-sm">
+              {components.Valunteer?.[language].discription2}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center gap-3  cursor-pointer">
+          <div className="w-44 h-44 lg:w-52 lg:h-60">
+            <img
+              src="avatar/22.jpg"
+              className="object-cover w-full h-full"
+              alt=""
+            />
+          </div>
+          <div className="flex flex-col items-center gap-0">
+            <div className="font-bold text-xs md:text-sm">
               {components.Valunteer?.[language].title4}
             </div>
-            <div className="text-xs md:text-lg">
+            <div className="text-xs md:text-sm">
               {components.Valunteer?.[language].discription4}
             </div>
           </div>
         </div>
 
         <div className="flex flex-col items-center gap-3  cursor-pointer">
-          <div className="w-72 h-72">
+          <div className="w-44 h-44 lg:w-52 lg:h-60">
             <img
               src="avatar/33.jpg"
               className="object-cover w-full h-full"
@@ -76,17 +76,17 @@ const Valunteer = ({ language }) => {
             />
           </div>
           <div className="flex flex-col items-center gap-0">
-            <div className="font-bold text-xs md:text-lg">
+            <div className="font-bold text-xs md:text-sm">
               {components.Valunteer?.[language].title3}
             </div>
-            <div className="text-xs md:text-lg">
+            <div className="text-xs md:text-sm">
               {components.Valunteer?.[language].discription3}
             </div>
           </div>
         </div>
 
         <div className="flex flex-col items-center gap-3  cursor-pointer">
-          <div className="w-72 h-72">
+          <div className="w-44 h-44 lg:w-52 lg:h-60">
             <img
               src="avatar/Capture.PNG"
               className="object-cover w-full h-full"
@@ -94,10 +94,10 @@ const Valunteer = ({ language }) => {
             />
           </div>
           <div className="flex flex-col items-center gap-0">
-            <div className="font-bold text-xs md:text-lg">
+            <div className="font-bold text-xs md:text-sm">
               {components.Valunteer?.[language].title5}
             </div>
-            <div className="text-xs md:text-lg">
+            <div className="text-xs md:text-sm">
               {components.Valunteer?.[language].discription5}
             </div>
           </div>
